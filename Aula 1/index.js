b393bde14e0d43d6faa655a00b61550aaccd6edc
Thspli <thalissondouglas86@gmail.com>
2025-02-11 10:45:58 -0300

    Aula 1

---
+++ Aula 1/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req, res) =>{
+res.send('Pagina Principal');
+})
+
+app.get('/home', (req, res) =>{
+    res.send('Pagina home');
+    })
+
+
+app.get('/login', (req, res) =>{
+        res.send('Pagina login');
+        })
+
+
+
+
+
+
+
+app.listen(port, ()=>{
+    console.log('Servidor rodando na porta ', port);
+})
