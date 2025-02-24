ebeb0a6aa40e1fbdf070a5c7f5ab5721bb84d4ff
Thspli <thalissondouglas86@gmail.com>
2025-02-24 17:05:55 -0300

    Banco de dados em json

--- Aula 3/userService.js
+++ Aula 3/userService.js
@@ -1,10 +1,31 @@
 const User = require ("./user");
+const path = require('path'); //modulo para manipular caminhos
+const fs = require('fs'); // modulo para manipular arquivos files system
 
 class userService{
     constructor(){
+        this.filePath = path.join(__dirname, 'user.json');
         this.users = []; //Array para armazenar usuário
         this.nextId = 1; //contador para gerar id
     }
+
+    loadUsers(){
+        try{
+        if(fs.existsSync(this.filePath)){//Verifica se o arquivo existe
+            const data = fs.readFileSync(this.filePath);//Le o arquivo
+            return JSON.parse(data);//transforma o json em objeto
+        }
+    }catch(erro){//Caso ocorra um erro
+        console.log("Erro ao carregar arquivo", erro);
+    }
+    return[];
+    }
+
+    getNextId(){
+        if(this.users.length===0) return 1;
+        return Math.max(...this.users.map(user => user.id))+1;
+   }
+    
 
         addUser(nome, email){
             const user = new User(this.nextId++, nome, email); //++ vai adicionar mais 1 no número do id a cada novo usuário, que inicialmente é 1.
